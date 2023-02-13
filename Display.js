6ab406c73f3ff448e662d0dd992276017a2d519b
RiadSaker <107034954+RiadSaker@users.noreply.github.com>
2023-02-13 01:08:53 +0100

    Create Display.js

---
+++ Display.js
@@ -0,0 +1,54 @@
+class SLLNode {
+    constructor(val) {
+        this.value = val;
+        this.next = null;
+    }
+}
+
+class SLL {
+    constructor() {
+        this.head = null;
+    }
+    addFront(value) {
+        var newNode = new SLLNode(value);
+        newNode.next = this.head;
+        this.head = newNode;
+        return this.head;
+    }
+    removeFront() {
+        if (this.head == null) {
+            return this.head;
+        }
+        var removedNode = this.head;
+        this.head = removedNode.next;
+        removedNode.next = null;
+        return this.head;
+    }
+    front() {
+        if (this.head == null) {
+            return null;
+        } else { // List is not empty
+            return this.head.value;
+        }
+    }
+    display() {
+        var listStr = "";
+        if (this.head == null) {
+            return "Empty list";
+        }
+        listStr += this.head.value;
+        var runner = this.head.next;
+        while (runner != null) {
+            listStr += ", " + runner.value;
+            runner = runner.next;
+        }
+        return listStr;
+    }
+}
+
+var mySLL = new SLL();
+mySLL.addFront(10);
+mySLL.addFront(5);
+mySLL.addFront(3);
+console.log(mySLL);
+console.log(mySLL.display());
